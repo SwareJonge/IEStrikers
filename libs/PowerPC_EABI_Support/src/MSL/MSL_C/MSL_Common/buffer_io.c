973e8dd0fc41bf1d3334bdac8ec1f2b3b102937a
SwareJonge <olaf23okken@gmail.com>
2023-04-23 02:14:04 +0200

    match __load_buffer

--- libs/PowerPC_EABI_Support/src/MSL/MSL_C/MSL_Common/buffer_io.c
+++ libs/PowerPC_EABI_Support/src/MSL/MSL_C/MSL_Common/buffer_io.c
@@ -1,8 +1,7 @@
+#define _MSL_WIDE_CHAR
 #include "PowerPC_EABI_Support/MSL_C/MSL_Common/ansi_files.h"
 
-
-void __prep_buffer(FILE* file)
-
+void __prep_buffer(FILE* file) 
 {
 	file->mBufferPtr      = file->mBuffer;
 	file->mBufferLength   = file->mBufferSize;
@@ -11,8 +10,44 @@ void __prep_buffer(FILE* file)
 	return;
 }
 
-void __load_buffer(){
-	// TODO
+int __load_buffer(FILE *file, u32 *n, int flag)
+{
+	int res;
+
+	__prep_buffer(file);
+
+	if (flag == 1) {
+		file->mBufferLength = file->mBufferSize;
+	}
+
+	res = (*file->readFunc)(file->mHandle, file->mBuffer, &file->mBufferLength, file->ref_con);
+	if(res == 2) {
+		file->mBufferLength = 0;
+	}
+
+	if (n) {
+		*n = file->mBufferLength;
+	}
+
+	if(res != 0) {
+		return res;
+	}
+
+	
+	file->mPosition += file->mBufferLength;
+
+	if (!file->mMode.binary_io)
+	{
+		int len = file->mBufferLength;
+		u8 *buf = file->mBuffer;
+		while(len--) {
+			if (*buf++ == '\n') {
+				file->mPosition++;
+			}
+		}
+	}
+
+	return 0;
 }
 
 int __flush_buffer(FILE* file, size_t* length)
@@ -33,10 +68,7 @@ int __flush_buffer(FILE* file, size_t* length)
 		file->mPosition += file->mBufferLength;
 	}
 
-	file->mBufferPtr      = file->mBuffer;
-	file->mBufferLength   = file->mBufferSize;
-	file->mBufferLength   = file->mBufferLength - (file->mPosition & file->mBufferAlignment);
-	file->mBufferPosition = file->mPosition;
+	__prep_buffer(file);
 	return 0;
 }
 
